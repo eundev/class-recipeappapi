9bdd190f189722dfba08301aee874121171641b8
Eun Lee <cs.eunlee@gmail.com>
2019-09-08 21:07:39 -0300

    Updated admin to support custom user model.

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -23,7 +24,7 @@ class ModelTests(TestCase):
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
-        #line below means that it passes the test if a ValueError occurs
+        """line below means that it passes the test if a ValueError occurs"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
@@ -36,4 +37,3 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        
